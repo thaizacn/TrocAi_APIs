a7b5b3b1ed142ff752955a4ccdb35e1a40260c3c
Thaizinha <thaizacarvalh@gmail.com>
2023-04-13 22:27:27 -0300

    Adjustments

--- integracaoAPIs/main.py
+++ integracaoAPIs/main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
 from datetime import datetime
-import implement
+import cadastroUsuario
+import registroProdutos
 
 app = FastAPI()
 
 @app.get("/login")
 def login(email: str, senha: str):
     # atribuir variavel para devolver o return do metodo
-    implement.login_usuario
+    cadastroUsuario.login_usuario
     return 
 
 @app.post("/cadastro")
 def cadastro(nome_completo: str, email: str, confirmacao_email: str, senha: str, confirmacao_senha: str):
     if email == confirmacao_email & senha == confirmacao_senha:
         # atribuir variavel para devolver o return do metodo
-        implement.cadastrar_usuario(nome_completo, email, senha)
+        cadastroUsuario.cadastrar_usuario(nome_completo, email, senha)
         return
     else:
         print("Email e senha não coincidem, favor revisar")
@@ -24,5 +25,5 @@ def registro_produto(item: str, descricao: str, data_entrada: str):
     #TO-DO amadurecer lógica para imagem
     data_formatada = datetime.strptime(data_entrada, "%d/%m/%Y")
     # atribuir variavel para devolver o return do metodo
-    implement.registrar_produto(item, descricao, data_entrada)
+    registroProdutos.registrar_produto(item, descricao, data_entrada)
     return 
