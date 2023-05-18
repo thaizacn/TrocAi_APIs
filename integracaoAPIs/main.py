25103c533655cc0aa12fe1a74ad403d6796eb367
Thaizinha <77704621+thaizacn@users.noreply.github.com>
2023-05-17 22:54:16 -0300

    New endpoints

--- integracaoAPIs/main.py
+++ integracaoAPIs/main.py
@@ -1,38 +1,43 @@
 from fastapi import FastAPI, HTTPException, status, UploadFile
-import cadastroUsuario
-import registroProdutos
-import avaliacoes
-import mensagens
+import services
 
 app = FastAPI()
 
 @app.get("/login")
 def login(email: str, senha: str):
-    return cadastroUsuario.login_usuario(email, senha)
+    return services.login_usuario(email, senha)
 
 @app.post("/cadastro")
 def cadastro(nome_completo: str, nome_usuario: str, email: str, confirmacao_email: str, senha: str, confirmacao_senha: str):
     if email == confirmacao_email and senha == confirmacao_senha:
         #TO-DO nome de usuário único: voltará none
-        return cadastroUsuario.cadastrar_usuario(nome_completo, nome_usuario, email, senha)
+        return services.cadastrar_usuario(nome_completo, nome_usuario, email, senha)
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email e senha não coincidem, favor revisar")
+    
+@app.get("/consulta_usuario")
+def consulta_usuario(email: str, senha: str):
+    return services.consulta_usuario(email, senha)    
 
 @app.post("/registro")
 def registro_produto(item: str, descricao: str, id: int, imagem: UploadFile = None):
-    return registroProdutos.registrar_produto(item, descricao, id, imagem)
+    return services.registrar_produto(item, descricao, id, imagem)
 
 @app.post("/mensagem")
 def mensagem(id: int, comentario: str, nota: int):
-    return mensagens.mensagem(id, comentario, nota)
+    return services.mensagem(id, comentario, nota)
 
 @app.post("/avaliacao_plataforma")
 def avaliacao_plataforma(id: int, comentario: str, nota: int):
-    return avaliacoes.avaliacao_plataforma(id, comentario, nota)
+    return services.avaliacao_plataforma(id, comentario, nota)
 
 @app.post("/avaliacao_operacao")
 def avaliacao_operacao(id: int, id_operacao: int, comentario: str, nota: int):
-    return avaliacoes.avaliacao_operacao(id, id_operacao, comentario, nota)
+    return services.avaliacao_operacao(id, id_operacao, comentario, nota)
+
+@app.get("/consulta_operacao")
+def consulta_operacao(id_usuario: int):
+    return services.consulta_operacao(id_usuario)
 
 if __name__ == "__main__":
     import uvicorn
--- integracaoAPIs/mensagens.py
+++
@@ -1,5 +0,0 @@
-import integracaoBD
-
-def mensagens():
-    integracaoBD.InclusaoBanco.adicionar_mensagem()
-    return {"message": "Mensagem adicionada com sucesso!"} 
